e0bf8ee8d56e047e278c0c31e5b6485523230256
heowonseo <heowonseo@naver.com>
2021-05-04 00:25:30 +0900

    upload files

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
 dotenv.config();
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const { sequelize } = require('./models');
@@ -29,12 +37,18 @@ sequelize.sync({ force: false })
     console.error(err);
   });
 
-app.use(morgan('dev'));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(helmet({ contentSecurityPolicy : false}));
+    app.use(hpp());
+  } else {
+    app.use(morgan('dev'));
+  }
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionOption = {
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -42,7 +56,13 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+  store: new RedisStore({ client: redisClient }),
+};
+if (process.env.NODE_ENV === 'production') {
+  sessionOption.proxy = true;
+  // sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -63,6 +83,4 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
-});
+module.exports = app;
--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const passport = require('passport');
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true});
+const cookieParser = require('cookie-parser');
 const User = require('../models/user');
 const router = express.Router();
 
@@ -10,32 +13,32 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/join', isNotLoggedIn, (req, res) => {
+router.get('/join', isNotLoggedIn, csrfProtection, (req, res) => {
   res.render('join', { title: '회원가입 - mileage' });
 });
-router.get('/manageInfo', (req, res, next) => {
+router.get('/manageInfo', csrfProtection, (req, res, next) => {
   User.findAll().then( result => {
     res.render('manageInfo', {
       users: result
     });
   });
 });
-router.get('/logout', (req, res) => {
+router.get('/logout', csrfProtection, (req, res) => {
   res.render('logout', { title: '로그아웃 - mileage' });
 });
-router.get('/manage',(req, res) => {
+router.get('/manage', csrfProtection, (req, res) => {
     res.render('manage', { title: '관리자 - mileage' });
   });
-router.get('/check', (req, res) => {
+router.get('/check', csrfProtection, (req, res) => {
     res.render('check', { title: '확인 및 수정-mileage' });
   });
-router.get('/success', (req, res) => {
+router.get('/success', csrfProtection, (req, res) => {
     res.render('success', { title: '성공-mileage' });
   });
-router.get('/getInform', (req, res) => {
+router.get('/getInform', csrfProtection, (req, res) => {
     res.render('getInform', { title: '확인-mileage'})
   });
-router.get('/', (req, res, next) => {
+router.get('/', csrfProtection, (req, res, next) => {
   res.render('main', { title: 'mileage' });
 });
 
